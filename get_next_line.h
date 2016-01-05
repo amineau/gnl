2f326e95b7c3ceaf86f5bf6bf812a6d67aaac876
Antoine MINEAU <amineau@e2r4p16.42.fr>
2016-01-05 19:02:36 +0100

    l

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amineau <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/17 11:38:15 by amineau           #+#    #+#             */
+/*   Updated: 2016/01/04 20:48:16 by amineau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 9999
+
+int	get_next_line(int const fd, char **line);
+
+#endif
